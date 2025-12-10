45fbb479273d383b02f8f1ae411347a97e3367fa
ja-gnahk <24520765@gm.uit.edu.vn>
2025-12-10 23:07:43 +0700

    Add files via upload

---
+++ BTTH04/Form1.Designer.cs
@@ -0,0 +1,277 @@
+﻿namespace BTTH04
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbFont = new System.Windows.Forms.ComboBox();
+            this.cbSize = new System.Windows.Forms.ComboBox();
+            this.ckbBold = new System.Windows.Forms.CheckBox();
+            this.ckbItalic = new System.Windows.Forms.CheckBox();
+            this.ckbUnderlined = new System.Windows.Forms.CheckBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rbRight = new System.Windows.Forms.RadioButton();
+            this.rbCenter = new System.Windows.Forms.RadioButton();
+            this.rbLeft = new System.Windows.Forms.RadioButton();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.btnColor = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // cbFont
+            // 
+            this.cbFont.FormattingEnabled = true;
+            this.cbFont.Location = new System.Drawing.Point(100, 69);
+            this.cbFont.Name = "cbFont";
+            this.cbFont.Size = new System.Drawing.Size(183, 24);
+            this.cbFont.TabIndex = 0;
+            this.cbFont.SelectedIndexChanged += new System.EventHandler(this.cbFont_SelectedIndexChanged);
+            // 
+            // cbSize
+            // 
+            this.cbSize.FormattingEnabled = true;
+            this.cbSize.Location = new System.Drawing.Point(394, 69);
+            this.cbSize.Name = "cbSize";
+            this.cbSize.Size = new System.Drawing.Size(82, 24);
+            this.cbSize.TabIndex = 1;
+            this.cbSize.SelectedIndexChanged += new System.EventHandler(this.cbSize_SelectedIndexChanged);
+            // 
+            // ckbBold
+            // 
+            this.ckbBold.AutoSize = true;
+            this.ckbBold.Location = new System.Drawing.Point(50, 116);
+            this.ckbBold.Name = "ckbBold";
+            this.ckbBold.Size = new System.Drawing.Size(18, 17);
+            this.ckbBold.TabIndex = 2;
+            this.ckbBold.UseVisualStyleBackColor = true;
+            this.ckbBold.CheckedChanged += new System.EventHandler(this.ckbBold_CheckedChanged);
+            // 
+            // ckbItalic
+            // 
+            this.ckbItalic.AutoSize = true;
+            this.ckbItalic.Location = new System.Drawing.Point(117, 117);
+            this.ckbItalic.Name = "ckbItalic";
+            this.ckbItalic.Size = new System.Drawing.Size(18, 17);
+            this.ckbItalic.TabIndex = 3;
+            this.ckbItalic.UseVisualStyleBackColor = true;
+            this.ckbItalic.CheckedChanged += new System.EventHandler(this.ckbItalic_CheckedChanged);
+            // 
+            // ckbUnderlined
+            // 
+            this.ckbUnderlined.AutoSize = true;
+            this.ckbUnderlined.Location = new System.Drawing.Point(169, 117);
+            this.ckbUnderlined.Name = "ckbUnderlined";
+            this.ckbUnderlined.Size = new System.Drawing.Size(18, 17);
+            this.ckbUnderlined.TabIndex = 4;
+            this.ckbUnderlined.UseVisualStyleBackColor = true;
+            this.ckbUnderlined.CheckedChanged += new System.EventHandler(this.ckbUnderlined_CheckedChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rbRight);
+            this.groupBox1.Controls.Add(this.rbCenter);
+            this.groupBox1.Controls.Add(this.rbLeft);
+            this.groupBox1.Font = new System.Drawing.Font("Arial", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(50, 158);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(164, 100);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Allign Text";
+            // 
+            // rbRight
+            // 
+            this.rbRight.AutoSize = true;
+            this.rbRight.Location = new System.Drawing.Point(18, 73);
+            this.rbRight.Name = "rbRight";
+            this.rbRight.Size = new System.Drawing.Size(58, 20);
+            this.rbRight.TabIndex = 12;
+            this.rbRight.TabStop = true;
+            this.rbRight.Text = "Right";
+            this.rbRight.UseVisualStyleBackColor = true;
+            this.rbRight.CheckedChanged += new System.EventHandler(this.rbRight_CheckedChanged);
+            // 
+            // rbCenter
+            // 
+            this.rbCenter.AutoSize = true;
+            this.rbCenter.Location = new System.Drawing.Point(18, 47);
+            this.rbCenter.Name = "rbCenter";
+            this.rbCenter.Size = new System.Drawing.Size(66, 20);
+            this.rbCenter.TabIndex = 11;
+            this.rbCenter.TabStop = true;
+            this.rbCenter.Text = "Center";
+            this.rbCenter.UseVisualStyleBackColor = true;
+            this.rbCenter.CheckedChanged += new System.EventHandler(this.rbCenter_CheckedChanged);
+            // 
+            // rbLeft
+            // 
+            this.rbLeft.AutoSize = true;
+            this.rbLeft.Location = new System.Drawing.Point(18, 21);
+            this.rbLeft.Name = "rbLeft";
+            this.rbLeft.Size = new System.Drawing.Size(49, 20);
+            this.rbLeft.TabIndex = 10;
+            this.rbLeft.TabStop = true;
+            this.rbLeft.Text = "Left";
+            this.rbLeft.UseVisualStyleBackColor = true;
+            this.rbLeft.CheckedChanged += new System.EventHandler(this.rbLeft_CheckedChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(243, 179);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(259, 53);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(45, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 23);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Font";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(334, 69);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 23);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Size";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(331, 110);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 23);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Color";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(75, 106);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(34, 33);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "B";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Arial", 16.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(141, 108);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(22, 32);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "I";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Arial", 16.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(193, 107);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 33);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "U";
+            // 
+            // btnColor
+            // 
+            this.btnColor.Location = new System.Drawing.Point(394, 106);
+            this.btnColor.Name = "btnColor";
+            this.btnColor.Size = new System.Drawing.Size(41, 27);
+            this.btnColor.TabIndex = 13;
+            this.btnColor.UseVisualStyleBackColor = true;
+            this.btnColor.Click += new System.EventHandler(this.btnColor_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(563, 309);
+            this.Controls.Add(this.btnColor);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.ckbUnderlined);
+            this.Controls.Add(this.ckbItalic);
+            this.Controls.Add(this.ckbBold);
+            this.Controls.Add(this.cbSize);
+            this.Controls.Add(this.cbFont);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbFont;
+        private System.Windows.Forms.ComboBox cbSize;
+        private System.Windows.Forms.CheckBox ckbBold;
+        private System.Windows.Forms.CheckBox ckbItalic;
+        private System.Windows.Forms.CheckBox ckbUnderlined;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RadioButton rbRight;
+        private System.Windows.Forms.RadioButton rbCenter;
+        private System.Windows.Forms.RadioButton rbLeft;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button btnColor;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+    }
+}
+
---
+++ BTTH04/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace BTTH04
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            FontFamily[] fontFamilies = FontFamily.Families;
+            foreach (FontFamily font in fontFamilies)
+            {
+                cbFont.Items.Add(font.Name);
+            }
+            cbFont.SelectedIndex = 0;
+            string[] sizes = { "8", "9", "10", "11", "12", "14", "16", "18", "20", "22", "24", "26", "28", "36", "48", "72" };
+            cbSize.Items.AddRange(sizes);
+            cbSize.SelectedItem = "28";
+
+            // Đặt mặc định Center và áp dụng Font ban đầu
+            rbCenter.Checked = true;
+            CapNhat();
+        }
+
+        private void btnColor_Click(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.ForeColor = colorDialog1.Color;
+                btnColor.BackColor = colorDialog1.Color;
+            }
+        }
+
+        private void CapNhat()
+        {
+            if (cbFont.SelectedItem == null || cbSize.SelectedItem == null) return;
+
+            string fontName = cbFont.SelectedItem.ToString();
+            float sizeName = float.Parse(cbSize.SelectedItem.ToString());
+
+            FontStyle fontstyle = FontStyle.Regular;
+
+            if (ckbBold.Checked) fontstyle = fontstyle | FontStyle.Bold;
+            if (ckbItalic.Checked) fontstyle = fontstyle | FontStyle.Italic;
+            if (ckbUnderlined.Checked) fontstyle |= FontStyle.Underline;
+
+            textBox1.Font = new Font(fontName, sizeName, fontstyle);
+        }
+
+        private void cbFont_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            CapNhat();
+        }
+
+        private void cbSize_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            CapNhat();
+        }
+
+        private void ckbBold_CheckedChanged(object sender, EventArgs e)
+        {
+            CapNhat();
+        }
+
+        private void ckbItalic_CheckedChanged(object sender, EventArgs e)
+        {
+            CapNhat();
+        }
+
+        private void ckbUnderlined_CheckedChanged(object sender, EventArgs e)
+        {
+            CapNhat();
+        }
+
+        private void rbLeft_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rbLeft.Checked)
+            {
+                textBox1.TextAlign = HorizontalAlignment.Left;
+            }
+        }
+        private void rbCenter_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rbCenter.Checked)
+            {
+                textBox1.TextAlign = HorizontalAlignment.Center;
+            }
+        }
+        private void rbRight_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rbRight.Checked)
+            {
+                textBox1.TextAlign = HorizontalAlignment.Right;
+            }
+        }
+    }
+}
